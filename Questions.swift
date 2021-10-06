2d6e9bfbeda09963ec2b9f5e94bac2989a8aab20
pranjalee03 <92021144+pranjalee03@users.noreply.github.com>
2021-10-06 19:55:20 +0530

    Add files via upload

---
+++ Questions.swift
@@ -0,0 +1,16 @@
+//
+//  Questions.swift
+//  Quizzler-iOS13
+//
+//  Created by Pranjalee on 28/07/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
+
+
